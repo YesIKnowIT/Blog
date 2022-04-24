811b3b69103765dd9351f6238a9a3e4dcb4926ec
Sylvain Leroux <sylvain@chicoree.fr>
2022-04-24 19:04:30 +0200

    Update worker.js

--- 2022/node-ipc/code/worker.js
+++ 2022/node-ipc/code/worker.js
@@ -1,5 +1,5 @@
 const str = process.argv[1];
 
-// (2) Load the rogue2 module and do some stuff
+// Load the rogue2 module and do some stuff
 const { addToDict } = require("./rogue2");
 console.log("addToDict(str):", addToDict(str))
