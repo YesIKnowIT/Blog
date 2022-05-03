4a1a23d7adfeaaa42f4f67ca849fd2652c7d168a
Sylvain Leroux <sylvain@chicoree.fr>
2022-05-03 11:05:55 +0200

    Final version of the program

--- 2022/intro-to-zig/code/wc-v2.zig
+++ 2022/intro-to-zig/code/wc-v2.zig
@@ -1,18 +1,17 @@
 //
-// compile and run this program by typing the following commands
+// Compile and run this program by typing the following commands
 // in your shell:
 //
-//      zig build-exe wc-v1.zig
-//      ./wc-v1
+//      zig build-exe wc-v2.zig
+//      ./wc-v2
 //
 
-// import the "std" module and make it available using the `std` namespace
+// Import the "std" module and make it available using the `std` namespace
 const std = @import("std");
 
-// few global variables,
+// Few global variables,
 // each one declared as a 32-bits unsigned integer
 var chars:u32 = 0;
-var words:u32 = 0;
 var lines:u32 = 0;
 
 // For now, our input is a hard-coded string
@@ -23,25 +22,41 @@ pub fn count() !void {
 
     // Count the chracters up to the end of the string
     var done = false;
-    var buffer: [1]u8 = undefined;
+    var buffer: [32*1024]u8 = undefined;
 
     while(!done) {
+        // Read enough data to fill the buffer
+        // The `try` keyword means: "If there is an error, abort this function and return the error"
         const nb = try stdin.readAll(&buffer);
-        if (nb == 0) {
+
+        // If we can't fill the buffer, that means we have exhausted the input stream
+        if (nb < buffer.len) {
             done = true;
         }
-        else {
+
+        // Iterate over the bytes of the buffer
+        for(buffer[0..nb]) |c| {
             chars += 1;
+
+            if (c == '\n') {
+                lines += 1;
+            }
+
+            // Uncomment the following three lines to simulate an error
+            // when we encounter the X character
+            // if (c == 'X') {
+            //     return error.Unexpected;
+            // }
         }
     }
 }
 
-// the main function is the starting point for your program's execution
+// The main function is the starting point for your program's execution
 pub fn main() void {
     count() catch |err| {
         std.debug.print("Error {} while reading file\n", .{ err });
     };
 
-    // call the function `print` of the `std.debug` namespace
-    std.debug.print(" {} chars, {} words, {} lines\n", .{ chars, words, lines });
+    // Call the function `print` of the `std.debug` namespace
+    std.debug.print(" {} lines, {} chars\n", .{ lines, chars });
 }
