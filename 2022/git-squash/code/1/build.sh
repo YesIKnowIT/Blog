a99f97f8aaf6c94e980b243533e56f68f5e875a0
Sylvain Leroux <sylvain@chicoree.fr>
2022-05-10 20:46:49 +0200

    Track orig revs

--- 2022/git-squash/code/1/build.sh
+++ 2022/git-squash/code/1/build.sh
@@ -5,6 +5,11 @@ DATE=1970-01-01T00:00:00
 COMMITS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 declare -i CIDX=0
 
+function head() {
+    LINK=$(git symbolic-ref --quiet HEAD || git rev-parse HEAD)
+    echo ${LINK##*/}
+}
+
 function commit() {
     sed -n '3,7p' "$1" | while IFS='' read; do
         echo "${REPLY}" >> "$2"
@@ -37,16 +42,25 @@ commit xx03 3.txt
 
 git checkout --quiet main
 
+ORIG_MAIN=$(git rev-parse main)
+ORIG_B1=$(git rev-parse B1)
+ORIG_B2=$(git rev-parse B2)
+ORIG_HEAD=$(head)
+
 {
-git reset --soft "${UPSTREAM}"
-git add 1.txt
-git commit -m "changed"
-git merge B1 -m "merge B1"
+#git reset --soft "${UPSTREAM}"
+git checkout B1~1
+#git checkout B1
+#git add 1.txt
+#git commit -m "changed"
+#git merge B1 -m "merge B1"
+:
 } > /dev/null
 
 MAIN=$(git rev-parse main)
 B1=$(git rev-parse B1)
 B2=$(git rev-parse B2)
+HEAD=$(head)
 
 {
     echo 'digraph G {'
@@ -55,37 +69,50 @@ B2=$(git rev-parse B2)
     echo 'margin = .25;'
     echo 'edge[penwidth=2];'
     echo 'node[style=filled fillcolor="#f1f1f1" penwidth=2 fontsize=12 fontname=notosans];'
+
     echo '"" [style="invis",width=0];'
-    git log --pretty='"%H" [label="%s" shape=circle width=.5];' main B1 B2
-    git log --pretty='"%H" -> "%P";' main B1 B2
+    git log --pretty='"%H" [label="%s" shape=circle width=.5];' $ORIG_MAIN $ORIG_B1 $ORIG_B2 $ORIG_HEAD main B1 B2 HEAD
+    git log --pretty='%H %P' $ORIG_MAIN $ORIG_B1 $ORIG_B2 $ORIG_HEAD main B1 B2 HEAD | while read H PS; do
+        for P in $PS; do
+            echo '"'$H'" -> "'$P'";'
+        done
+    done
+
+    echo 'subgraph cluster_root {'
+    echo 'rankdir="TB";'
 
     echo 'subgraph cluster_main {'
     echo 'label = "main";'
     echo 'style = "invis";'
     echo '"";'
-    git log --pretty='"%H";' main
+    git log --pretty='"%H";' $ORIG_MAIN
     echo '}'
 
     echo 'subgraph cluster_b1 {'
     echo 'label = "B1";'
     echo 'style = "invis";'
-    git log --pretty='"%H";' "${UPSTREAM}..B1"
+    git log --pretty='"%H";' "${UPSTREAM}..$ORIG_B1"
     echo '}'
 
     echo 'subgraph cluster_b2 {'
     echo 'label = "B2";'
     echo 'style = "invis";'
-    git log --pretty='"%H";' "${UPSTREAM}..B2"
+    git log --pretty='"%H";' "${UPSTREAM}..$ORIG_B2"
     echo '}'
 
     echo '"B1" -> "'"${B1}"'"'
     echo '"main" -> "'"${MAIN}"'"'
     echo '"B2" -> "'"${B2}"'"'
+    echo '"HEAD" -> "'"${HEAD}"'"'
+    echo 'B1[shape = box width = .75 style = "filled"];'
+    echo 'B2[shape = box width = .75 style = "filled"];'
+    echo 'main[shape = box width = .75 style = "filled"];'
+    echo 'HEAD[shape = box width = .75 style = "filled"];'
     echo '{'
-#    echo 'rank = same;'
-    echo 'B1[shape=box width=.75 style="filled"];'
-    echo 'B2[shape=box width=.75 style="filled"];'
-    echo 'main[shape=box width=.75 style="filled"];'
+    echo 'rank = same;'
+    echo '"'$ORIG_MAIN'" -> "'$ORIG_B1'" -> "'$ORIG_B2'" [style = invis];'
+    echo '}'
+
     echo '}'
 
     echo '}'
