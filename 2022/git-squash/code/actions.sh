f26c0ccbeef4507df58227f59966daa7278e07e6
Sylvain Leroux <sylvain@chicoree.fr>
2022-05-11 11:51:10 +0200

    Working version

--- 2022/git-squash/code/actions.sh
+++ 2022/git-squash/code/actions.sh
@@ -1,5 +1,22 @@
 git reset --soft "${UPSTREAM}"
-save 1.png
+save 1-1.png
 
 git commit -m "F...J"
-save 2.png
+save 1-2.png
+
+git merge --squash B2 
+git commit -m "K-O"
+save 2-1.png
+
+git checkout B2
+save 3-1.png
+
+export GIT_SEQUENCE_EDITOR='sed -i -e 2,5s/pick/squash/g'
+export VISUAL='true'
+git rebase -i "${UPSTREAM}"
+git commit --amend -m "P...T"
+save 3-2.png
+
+git checkout main
+git merge B2 -m "merge"
+save 3-3.png
