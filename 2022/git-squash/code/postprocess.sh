18b5e96a3b7f8aa19d3b5c3e3ec2d8aef6237bd5
Sylvain Leroux <sylvain@chicoree.fr>
2022-05-12 15:26:22 +0200

    First draft

--- 2022/git-squash/code/postprocess.sh
+++ 2022/git-squash/code/postprocess.sh
@@ -6,7 +6,7 @@ XWIDTH=1000
 cd tmp
 for f in ???.png; do
     INFILE="$f"
-    OUTFILE="../$f"
+    OUTFILE="../../$f"
     convert "${INFILE}" -gravity west -extent "${XWIDTH}" -background white -flatten -resize "${WIDTH}" "${OUTFILE}"
 done
 
