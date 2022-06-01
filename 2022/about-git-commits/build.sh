2a7cde319e6ad188f98bb5acaaeeac0e56e03d3d
Sylvain Leroux <sylvain@chicoree.fr>
2022-06-01 11:18:05 +0200

    Use PNG for embedded illustrations

--- 2022/about-git-commits/build.sh
+++ 2022/about-git-commits/build.sh
@@ -8,17 +8,27 @@ BUILDSET=(
 
 for BUILD in "${BUILDSET[@]}"; do
     IFS=: read -a LAYERS <<< "$BUILD"
-    OUT="${LAYERS[0]}"
+    SVG="git-objects-database-${LAYERS[0]}.svg"
     unset LAYERS[0]
     ARGS=""
     for LAYER in "${LAYERS[@]}"; do
         ARGS="$ARGS -u //svg:g[@inkscape:groupmode='layer'][@inkscape:label='$LAYER']/@style -v display:inline"
     done
+    
+    (
+      xmlstarlet ed -P -S < master/git-objects-database.svg > "$SVG" \
+        -N inkscape="http://www.inkscape.org/namespaces/inkscape" \
+        -N svg="http://www.w3.org/2000/svg" \
+        -d '//svg:g[@inkscape:groupmode="layer"]/@style' \
+        -i '//svg:g[@inkscape:groupmode="layer"]' -t attr -n style -v 'display:none' \
+        $ARGS
 
-    xmlstarlet ed -P -S < master/git-objects-database.svg > "git-objects-database-$OUT.svg" \
-      -N inkscape="http://www.inkscape.org/namespaces/inkscape" \
-      -N svg="http://www.w3.org/2000/svg" \
-      -d '//svg:g[@inkscape:groupmode="layer"]/@style' \
-      -i '//svg:g[@inkscape:groupmode="layer"]' -t attr -n style -v 'display:none' \
-      $ARGS
+        inkscape "$SVG" --export-png="${SVG%.*}.png" \
+                        --export-area-page --export-dpi=96 \
+                        --export-background='#fff' --export-background-opacity=0.0
+
+        rm "$SVG"
+    )&
+
 done
+wait
